10937f3c62f54dbdc4106eef3b54eed20b80fcf1
Michael Shennan <m.shennan22@gmail.com>
2017-04-11 19:34:33 +0100

    added digitalcamera, analogcamera and photographer classes

---
+++ Photographer.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Photographer {
+  
+  private String name;
+  private ArrayList<Printable> collection;
+
+  public Photographer(String name) {
+    this.name = name;
+    this.collection = new ArrayList<Printable>();
+  }
+
+}
